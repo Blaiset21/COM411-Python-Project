1e57b1ae9da6aa51e80829992419c24370210457
Blaise Tyree <5tyreb73@solent.ac.uk>
2021-11-04 09:50:57 +0000

    Added code to demonstrate how to index a list.

--- data/lists/Simple_list.py
+++ data/lists/Simple_list.py
@@ -1,11 +1,12 @@
 def directions():
-    directions = ["Move Forward", "Move Backward", "Turn Left", "Turn Right"]
-    return directions
+    directions1 = ["Move Forward", "Move Backward", "Turn Left", "Turn Right"]
+    return directions1
 
 
 def run():
     directions()
     print(directions())
+
 
 if __name__ == "__main__":
     run()

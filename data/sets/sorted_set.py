7f6236c3934e21490be46a300b412e38eea6333e
Blaise Tyree <5tyreb73@solent.ac.uk>
2021-11-25 09:54:55 +0000

    Added code to demonstrate how to manipulate and sort a set.

--- data/sets/sorted_set.py
+++ data/sets/sorted_set.py
@@ -1,15 +1,29 @@
 def observed():
     observations = []
-    for count in range(7):
+    for count in range(5):
         print("Please enter an observation:")
         observe = input()
         observations.append(observe)
     return observations
 
 
+def remove_observations(observations):
+    is_running = True
+    while(is_running):
+        print("Do you wish to remove an observation (yes/no)?")
+        yn = input()
+        if yn == "yes":
+            print("What observation do you wish to remove?")
+            gone = input()
+            observations.remove(gone)
+        else:
+            is_running = False
+
+
 def run():
     print("Counting observations...")
     store = observed()
+    remove_observations(store)
     see = set()
     for observation in store:
         data = (observation, store.count(observation))
